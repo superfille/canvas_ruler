143e7c2811d6a3111096416f621b33b16cd84017
DESKTOP-NE9F1E8\filip <superfilles12@gmail.com>
2017-01-22 14:11:34 +0100

    added pixelize

--- ruler/ruler.js
+++ ruler/ruler.js
@@ -1,15 +1,16 @@
+
 class Ruler {
-    constructor(canvas, side, font = 'Arial', fontSize = 10, rulerHeight = 15) {
+    constructor(parent, canvas, side, font = 'Arial', fontSize = 10, rulerHeight = 15) {
         this.side = side;
         this.canvas = canvas;
         this.context = canvas.getContext('2d');
-        this.canvas.style.left = this.side == 'top' ? this._pixelize(rulerHeight) : this._pixelize(-rulerHeight);
+        this.canvas.style.left = this.side == 'top' ? rulerHeight + 'px' : -rulerHeight + 'px';
         this.pointThickness = rulerHeight;
         this.scale = 1;
         this.longPoint = 0;
         this.mediumPoint = this.pointThickness * 0.25;
         this.smallPoint = this.pointThickness * 0.5;
-        this.context.font = this._pixelize(fontSize) + ' ' + font;
+        this.context.font = fontSize + 'px ' + font;
         this.lineWidth = 1;
         this.orgLineWidth = 1;
         this.zeroPositions = {x: 0, y: 0};
@@ -17,9 +18,9 @@ class Ruler {
 
         if(this.side == 'left') {
             this._rotate(90);
-            this.canvas.style.top = this._pixelize(rulerHeight);
+            this.canvas.style.top = rulerHeight + 'px';
         }
-        this.parent = $('#rulerArea')[0];
+        this.parent = parent;
         this.tracker = this._createTracker();
     }
 
@@ -42,9 +43,9 @@ class Ruler {
 
     updateMousePosition(position) {
         if(this.side == 'top') {
-            this.tracker.css('left', this._pixelize((position.x - this.parent.offsetLeft)));
+            this.tracker.css('left', ((position.x - this.parent.offsetLeft))  + 'px');
         }else {
-            this.tracker.css('top', this._pixelize((position.y - this.parent.offsetTop + this.pointThickness)));
+            this.tracker.css('top', ((position.y - this.parent.offsetTop + this.pointThickness)) + 'px');
         }
     }
 
@@ -57,11 +58,12 @@ class Ruler {
         this._createObjectTrackers();
 
         if(this.side == 'top') {
-            this.objTracker1.css('left', this._pixelize((position.x * this.scale + this.pointThickness)));
-            this.objTracker2.css('left', this._pixelize((position.x2 * this.scale + this.pointThickness)));
+            this.objTracker1.style.left = 
+            this.objTracker1.css('left', ((position.x * this.scale + this.pointThickness))  + 'px');
+            this.objTracker2.css('left', ((position.x2 * this.scale + this.pointThickness))  + 'px');
         }else {
-            this.objTracker1.css('top', this._pixelize((position.y * this.scale  + this.pointThickness)));
-            this.objTracker2.css('top', this._pixelize((position.y2 * this.scale + this.pointThickness)));
+            this.objTracker1.css('top', ((position.y * this.scale  + this.pointThickness)) + 'px');
+            this.objTracker2.css('top', ((position.y2 * this.scale + this.pointThickness)) + 'px');
         }
     }
 
@@ -197,64 +199,14 @@ class Ruler {
     }
 
     _drawText(x, y, text) {
-        /*
-         * In order to keep the text the same size all the time
-         * we save the context change the transform and then restore the context.
-         */
         this.context.save();
         this.context.setTransform(1, 0, 0, 1, 0, 0);
         this.context.fillText(text, x * this.scale, y);
         this.context.restore();
     }
 
-    _rotate(angle) {
-        var rotation = 'rotate(' + angle + 'deg)';
-        var origin = Math.abs(parseInt(this.canvas.style.left)) + 'px' + ' 100%';
-        this.canvas.style.webkitTransform = rotation;
-        this.canvas.style.MozTransform = rotation;
-        this.canvas.style.OTransform = rotation;
-        this.canvas.style.msTransform = rotation;
-        this.canvas.style.transform = rotation;
-        this.canvas.style.webkitTransformOrigin = origin;
-        this.canvas.style.MozTransformOrigin = origin;
-        this.canvas.style.OTransformOrigin = origin;
-        this.canvas.style.msTransformOrigin = origin;
-        this.canvas.style.transformOrigin = origin;
-    }
-
     _downScale(value) {
         return this.scale < 1 ? (1 / this.scale) : value;
-    }
-
-    _createObjectTrackers() {
-        if(this.objTracker1 === undefined || this.objTracker1 === null || this.objTracker2 === undefined || this.objTracker2 === null) {
-            this._removeObjectTracker();
-            this.objTracker1 = this._createTracker();
-            this.objTracker2 = this._createTracker();
-        }
-    }
-
-    _removeObjectTracker() {
-        if(this.objTracker1 !== undefined && this.objTracker1 !== null) {
-            this.objTracker1.remove();
-            this.objTracker1 = null;
-        }
-
-        if(this.objTracker2 !== undefined && this.objTracker2 !== null) {
-            this.objTracker2.remove();
-            this.objTracker2 = null;
-        }
-    }
-
-    _createTracker() {
-        var s = this.side == 'top' ? 'height' : 'width';
-        if(this.side == 'top') {
-            s = 'height:' + this.pointThickness + 'px;' + ' top: 0px';
-        }else {
-            s = 'width:' + this.pointThickness + 'px;' + ' left: 0px';
-        }
-
-        return $('<div class="ruler_guide" style="' + s + '"></div>').appendTo(this.parent);
     }
 
     _pixelize(value) {
